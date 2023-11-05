fa4855133933318cd46eabd0cbb0690d6dd339fd
Sashaperekhrest <chebupelmen20@gmail.com>
2023-11-05 22:30:15 +0500

    update

--- Test_6/src/generator/TimeDependetIntGenerator.java
+++ Test_6/src/generator/TimeDependetIntGenerator.java
@@ -4,14 +4,14 @@ public class TimeDependetIntGenerator implements intGenerator{
     @Override
     public int nextPositive() {
         double time = System.currentTimeMillis();
-        int out = (int) ((time % 1987)/1987 * 1000);
+        int out = (int) ((time % 10001)/10001 * 1000);
         return out;
     }
 
     @Override
     public int nextNegative() {
         double time = System.currentTimeMillis();
-        int out = (int) ((time % 1987)/1987 * (-1000));
+        int out = (int) ((time % 10001)/10001 * (-1000));
         return out;
     }
 
