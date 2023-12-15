54d28cc8914c15dca8ba2eaa7e88fd791fdd2c15
Sashaperekhrest <chebupelmen20@gmail.com>
2023-12-15 16:37:52 +0500

    Add test_spring project

---
+++ test_spring/src/test/java/softbyhumans/test_spring/TestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package softbyhumans.test_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
